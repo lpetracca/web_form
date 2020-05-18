b6cf64d4e78d1d6e3be9555424a39b2b6835bbfe
lpetracca <lucia.petracca@yahoo.com>
2020-05-17 22:20:50 -0300

    fixed child toggle

--- js/modules/form-by-steps/form_events.js
+++ js/modules/form-by-steps/form_events.js
@@ -34,53 +34,7 @@ export default class Form_Events {
         return goToStep;
     }
  
-/*     goToStep(step, direction = 'next') {
-        // 2) Simplifica esta función para que sean menos líneas.
-        let currentStep = step.replace(/^step\-/, '');
-        let goToStep = '.step-';
-        switch (currentStep) {
-            case '1':
-                goToStep += '2';
-                break;
-            case '2':
-                if ('prev' === direction) {
-                    goToStep += '1';
-                } else {
-                    goToStep += '3';
-                }
-                break;
-            case '3':
-                goToStep += '2';
-                break;
-            default:
-                break;
-        }
-        return goToStep;
-    } */
-
-  /*
-    previousAction(evt) {
-        let $current = $(evt.currentTarget);
-        let $formStep = $current.parents('.form-step');
-        
-        $formStep.addClass('d-none');
-
-        let $prevStep = $(this.goToStep($formStep[0].classList[1], 'prev'));
-        $prevStep.removeClass('d-none');
-    }
-
-    nextAction(evt) {
-        // 3) ¿Se puede evitar repetir mismas líneas que en previousAction?
-        let $current = $(evt.currentTarget);
-        let $formStep = $current.parents('.form-step');
-        
-        $formStep.addClass('d-none');
-
-        let $nextStep = $(this.goToStep($formStep[0].classList[1]));
-        $nextStep.removeClass('d-none');
-    } */
- 
-    changeAction(evt,direction='next'){
+  changeAction(evt,direction='next'){
         let $current = $(evt.currentTarget);
         let $formStep = $current.parents('.form-step');
         
@@ -96,14 +50,13 @@ export default class Form_Events {
     nextAction(evt) {this.changeAction(evt);}
 
     progressBar(percent) {
-        // 1) Escribir aqui como sería la lógica para incrementar la barra de porcentaje.
         let $progressBar = $(".progress-bar");
         $progressBar.css("width", percent + "%");
         $progressBar.html(percent + "%");
         }
     }
     
-   function getFormData(formId){
+   function getFormData(formId) {
         let formValues = {};
         var form1Inputs = document.forms[formId].getElementsByTagName("input");
         for(let i=0; i<form1Inputs.length; i++){
--- js/script.js
+++ js/script.js
@@ -5,39 +5,8 @@ class Main extends Form {
     constructor() {
         super();
         console.log('Script init');
-        let $select = $(".children");
-
-        function toggler (){
-            console.log('aca estoy');
-            
-            let $childrenData = $('.childrenData');
-            let childValue = $select.val();
-            
-            switch(childValue) {
-            case '0':
-              $childrenData.hide();
-              break;                  
-                
-            case '1':
-                $('.child2').hide();  
-                $('.child3').hide();
-                $('.child4').hide();
-            break;
-            case '2':
-                $('.child3').hide();
-                $('.child4').hide();
-                break;  
-            case '3':
-                $('.child4').hide();
-                break;
-            case '4':
-                break;
-            }
-                       
-        };
-
     }
-
+   
 }
 
 let main = new Main();
